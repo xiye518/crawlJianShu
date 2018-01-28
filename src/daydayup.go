1850db8f761a0f4a288afad770cc2905090016b1
xiye <xiye518@gmail.com>
2018-01-28 17:54:50 +0800

    添加原生爬取

---
+++ src/daydayup.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"tools/console/color"
+)
+
+func init() {
+	color.NoColor = false
+}
+
+func main() {
+	var a, b = 1.00, 1.01
+	var count int
+	for a < 2.00 {
+		count++
+		a *= b
+		fmt.Println(b,a)
+	}
+	
+	color.Println(color.HiCyan("\nGet A little bit of progress everyday,how many days will it take to double:"), color.HiGreen(count)) //70
+	//fmt.Println("\nGet A little bit of progress everyday,how many days will it take to double:", count) //70
+}
--- src/main.go
+++ src/main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 	"errors"
 	"tools/console/color"
-	"regexp"
+	"model"
 )
 
 func init() {
@@ -27,7 +27,7 @@ func main() {
 	//color.LogAndPrintln(color.HiGreen(body))
 	//ToDo  也可以用ioutil将文本写出
 	//此处为使用正则从文本中切取想要的内容
-	arts, err := getArticles(body)
+	arts, err := model.ParseArticles(body)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -63,34 +63,4 @@ func searchJianShuHome(httpClient *http.Client) (body string, err error) {
 	body = string(bytes)
 	
 	return body, nil
-}
-
-func getArticles(body string) (arts []*Article, err error) {
-	arts = make([]*Article, 0)
-	//reg:=regexp.MustCompile(`(?s)<a class="title" target="_blank" href="(.+?)">(.+?)</a>`)
-	reg := regexp.MustCompile(`(?s)<a class="title" target="_blank" href="(.+?)">(.+?)</a>\s*<p class="abstract">\s*(.+?)</p>`)
-	result := reg.FindAllStringSubmatch(body, -1) //...		/p/6603d0ad230f	大脑版本升级：练习三个思维模型，一下午就能让你聪明起来  描述...
-	for _, r := range result {
-		var a Article
-		a.Url = r[1]
-		a.Title = r[2]
-		a.Abstract = r[3]
-		arts = append(arts, &a)
-	}
-	
-	return arts, err
-}
-
-type Article struct {
-	Title      string
-	AUthor     string
-	Abstract   string
-	Url        string
-	Watched    string //已阅
-	Comment    string //点评数
-	Collection string //收藏数
-}
-
-func (a *Article) String(i int){
-	color.LogAndPrintln(i, color.HiGreen(a.Title), "https://www.jianshu.com"+a.Url,a.Abstract)
 }
